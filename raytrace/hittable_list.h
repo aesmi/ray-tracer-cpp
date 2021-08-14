3a6b06b3d0157b75283c60c3352942ee289fe8eb
zc <huraisankgy@gmail.com>
2021-08-14 22:12:29 +0800

    await render

--- raytrace/hittable_list.h
+++ raytrace/hittable_list.h
@@ -1,15 +1,25 @@
 #ifndef HITTABLE_LIST_H
 #define HITTABLE_LIST_H
+//==============================================================================================
+// Originally written in 2016 by Peter Shirley <ptrshrl@gmail.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and related and
+// neighboring rights to this software to the public domain worldwide. This software is
+// distributed without any warranty.
+//
+// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
+// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//==============================================================================================
+
+#include "rtweekend.h"
 
 #include "hittable.h"
 
 #include <memory>
 #include <vector>
 
-using std::shared_ptr;
-using std::make_shared;
 
-class hittable_list : public hittable {
+class hittable_list : public hittable  {
     public:
         hittable_list() {}
         hittable_list(shared_ptr<hittable> object) { add(object); }
@@ -24,9 +34,10 @@ class hittable_list : public hittable {
         std::vector<shared_ptr<hittable>> objects;
 };
 
+
 bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
     hit_record temp_rec;
-    bool hit_anything = false;
+    auto hit_anything = false;
     auto closest_so_far = t_max;
 
     for (const auto& object : objects) {
@@ -40,5 +51,5 @@ bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& re
     return hit_anything;
 }
 
-#endif
 
+#endif
--- raytrace/sphere.h
+++ raytrace/sphere.h
@@ -1,13 +1,27 @@
 #ifndef SPHERE_H
 #define SPHERE_H
+//==============================================================================================
+// Originally written in 2016 by Peter Shirley <ptrshrl@gmail.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and related and
+// neighboring rights to this software to the public domain worldwide. This software is
+// distributed without any warranty.
+//
+// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
+// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//==============================================================================================
+
+#include "rtweekend.h"
 
 #include "hittable.h"
-#include "vec3.h"
+
 
 class sphere : public hittable {
     public:
         sphere() {}
-        sphere(point3 cen, double r) : center(cen), radius(r) {};
+
+        sphere(point3 cen, double r, shared_ptr<material> m)
+            : center(cen), radius(r), mat_ptr(m) {};
 
         virtual bool hit(
             const ray& r, double t_min, double t_max, hit_record& rec) const override;
@@ -15,7 +29,9 @@ class sphere : public hittable {
     public:
         point3 center;
         double radius;
+        shared_ptr<material> mat_ptr;
 };
+
 
 bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
     vec3 oc = r.origin() - center;
@@ -39,8 +55,10 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
     rec.p = r.at(rec.t);
     vec3 outward_normal = (rec.p - center) / radius;
     rec.set_face_normal(r, outward_normal);
+    rec.mat_ptr = mat_ptr;
 
     return true;
 }
+
 
 #endif
